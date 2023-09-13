0b5045183d8c384ea10dc86741d4951fc77dd161
Aaron Mamparo <aaronmamparo@gmail.com>
2023-09-13 07:45:05 -0500

    multiples

--- src/main.py
+++ src/main.py
@@ -1,4 +1,4 @@
-from typing import Set, Dict
+from typing import Set, Dict, List, Union
 
 from src.cards import get_cards, Card, Rarity, Color
 
@@ -7,13 +7,13 @@ N = 48
 cards: Set[Card] = {x for x in get_cards() if len(x.colors) <= 2}
 
 
-def get_subcube(rarities: Set[Rarity], n: int) -> Set[Card]:
+def get_subcube(rarities: Set[Rarity], n_unique: int, n_of_each: int) -> List[Card]:
     available_cards: Set[Card] = {x for x in cards if x.rarity in rarities}
     optimal_distribution = __distribution(available_cards)
 
-    subcube: Set[Card] = set()
+    subcube: List[Card] = []
 
-    while len(subcube) < n:
+    while len(subcube) < n_unique * n_of_each:
         current_distribution = __distribution(subcube)
         distribution_distances = {c: v - current_distribution.get(c, 0) for c, v in optimal_distribution.items()}
         high_distribution_distance = max(distribution_distances.values())
@@ -22,17 +22,18 @@ def get_subcube(rarities: Set[Rarity], n: int) -> Set[Card]:
             [x for x in available_cards if candidate_colors.intersection(x.colors)],
             key=lambda x: x.rating, reverse=True
         )[0]
-        subcube.add(next_card)
+        for _ in range(n_of_each):
+            subcube.append(next_card)
         available_cards.remove(next_card)
 
     return subcube
 
 
 def main():
-    cube: Set[Card] = set()
-    cube.update(get_subcube({Rarity.RARE, Rarity.MYTHIC}, 24))
-    cube.update(get_subcube({Rarity.UNCOMMON}, 48))
-    cube.update(get_subcube({Rarity.COMMON}, 80))
+    cube: List[Card] = []
+    cube.extend(get_subcube({Rarity.RARE, Rarity.MYTHIC}, 24, 1))
+    cube.extend(get_subcube({Rarity.UNCOMMON}, 48, 2))
+    cube.extend(get_subcube({Rarity.COMMON}, 80, 3))
 
     for color in {item for sublist in [x.colors for x in cards] for item in sublist}:
         color_cards = sorted([x for x in cube if color in x.colors], key=lambda x: x.rating, reverse=True)
@@ -41,7 +42,7 @@ def main():
             print(f'{card.name} ({card.rating})')
 
 
-def __distribution(_cards: Set[Card]) -> Dict[Color, float]:
+def __distribution(_cards: Union[Set[Card], List[Card]]) -> Dict[Color, float]:
     colors = {}
     for card in _cards:
         for color in card.colors:
